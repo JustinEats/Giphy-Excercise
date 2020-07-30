2e3756dbc798a465461a5fdbd43a7d423c28b3c1
Justin Z <contactjustinzamora@gmail.com>
2020-07-29 22:17:12 -0700

    added remove button and minor changes

--- app.js
+++ app.js
@@ -10,29 +10,29 @@ async function searchGiphy(searchGif) {
 		const res = await axios.get('http://api.giphy.com/v1/gifs/search', {
 			params: { q: searchGif, api_key: 'MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym' }
 		});
-		imageInput(res); //Pass in the request inside to invoke
+		imageInput(res); //Pass in the request inside to invoke appending
 	} catch (e) {
 		console.log('Search cannot be found');
 	}
 }
 function imageInput(response) {
+	//Create a function to seperate DOM manipluation
 	const { data } = response.data;
 	const $image = $('<img>');
 	$image.attr({
 		src: data[0].images.original.url,
-		class: 'w-50'
+		class: 'w-50 text-center rounded'
 	});
 	$giphyHere.append($image);
 	console.log(data[0].images.original.url);
 }
 
-function randomIndex(random) {
-	const randomIdx = random.length;
-	Math.floor(Math.random() * randomIdx);
-}
-
 $form.on('submit', function(e) {
 	e.preventDefault();
-	searchGiphy($input.val());
-	$input.val('');
+	searchGiphy($input.val()); //pass in request here with the value input as argument
+	$input.val(''); //empty the value after submitted
+});
+
+$buttonRemove.on('click', function() {
+	$giphyHere.empty();
 });
